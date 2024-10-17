dfd38f9cf1879abf846e61d8d69c3f3a9b97996f
David R <elidovrich.coding@gmail.com>
2024-10-17 14:30:40 -0400

    complete lesson 5

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "../../public/dojo-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        width={70}
+        quality={100}
+        placeholder="blur"
+        alt="logo"
+      />
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
